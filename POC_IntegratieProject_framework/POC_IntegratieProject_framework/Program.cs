45ab2b62f8ed1f1d1aa6bd5ec3395815762ba8d7
potathomas <thomas.somers@live.nl>
2018-03-27 11:52:23 +0200

    TestMetJoren

--- POC_IntegratieProject_framework/POC_IntegratieProject_framework/Program.cs
+++ POC_IntegratieProject_framework/POC_IntegratieProject_framework/Program.cs
@@ -19,7 +19,7 @@ namespace PolitiekeBarometer_CA
         private static IPlatformManager platformManager;
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("testThomas");
             elementManager = new ElementManager();
             postManager = new PostManager();
             dashboardManager = new DashboardManager();
